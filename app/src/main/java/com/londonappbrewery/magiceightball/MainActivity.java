097e19a70c37b4b5e33b0645c18ed0f33e5436ea
NARAYAN <naranthakkar@gmail.com>
2021-04-11 16:24:45 +0530

    Update App to functionality to perform as required.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button ask = findViewById(R.id.Askbutton);
+        final ImageView viewer = findViewById(R.id.ResultView);
+        final int[] drawable = {R.drawable.ball1,
+                          R.drawable.ball2,
+                          R.drawable.ball3,
+                          R.drawable.ball4,
+                          R.drawable.ball5};
+        ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomGenerator = new Random();
+                viewer.setImageResource(drawable[randomGenerator.nextInt(5)]);
+            }
+        });
     }
 }
